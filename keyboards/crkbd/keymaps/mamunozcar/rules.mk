937df80e3914707d358f7cab25050abb2041345b
mamunozcar <m.munozcarr@gmail.com>
2025-12-08 23:20:20 -0300

    feat: add base configuration

---
+++ keyboards/crkbd/keymaps/mamunozcar/rules.mk
@@ -0,0 +1,22 @@
+# Enable combos
+COMBO_ENABLE = yes
+
+# Enable OLED if available
+OLED_ENABLE = yes
+
+# Enable WPM counter for bongocat animation
+WPM_ENABLE = yes
+
+# Enable encoder map
+ENCODER_MAP_ENABLE = yes
+
+# Disable Tap Dance (causes lag)
+TAP_DANCE_ENABLE = no
+
+# Optimize firmware size
+LTO_ENABLE = yes
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no
+MAGIC_ENABLE = no
